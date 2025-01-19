d9467400fe4730f9fd422fb35f3e452f9847f875
SimonXVB <167611089+SimonXVB@users.noreply.github.com>
2025-01-19 21:35:46 +0100

    hero done

---
+++ arrows.js
@@ -0,0 +1,16 @@
+const arrows = document.getElementsByClassName("item");
+const buttons = document.getElementsByClassName("hero-button");
+
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("mouseover", () => {
+        for(let j = 0; j < arrows.length; j++) {
+            arrows[j].style.animation = `arrows 1s linear ${j / 6}s infinite`
+        }
+    });
+
+    buttons[i].addEventListener("mouseout", () => {
+        for(let j = 0; j < arrows.length; j++) {
+            arrows[j].style.animation = "none"
+        }
+    });
+};
